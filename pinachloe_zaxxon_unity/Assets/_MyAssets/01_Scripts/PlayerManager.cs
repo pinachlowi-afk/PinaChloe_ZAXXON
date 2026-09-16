0a82115b048dd4da36d2d868ccd1c7c209e6ba8f
CHLOE_8 <pinachlowi@gmail.com>
2026-09-16 12:23:28 +0200

    move x y shoot

---
+++ pinachloe_zaxxon_unity/Assets/_MyAssets/01_Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    bool isAlive;
+    public float speed;
+    [SerializeField] float lateralSepeed;
+    InputActions inputActions;
+    float MoveX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    void Shoot()
+    {
+
+        print("BOOOOM");
+            }
+    private void Awake()
+    {
+        inputActions = new InputActions();
+       ;
+        inputActions.player.Shoot.started += _ => Shoot();
+        inputActions.player.MoveX.performed += ctx => MoveX = ctx.ReadValue<float>();
+        inputActions.player.MoveX.canceled += _ => MoveX = 0f;
+
+    }
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right* Lateralspeed * Time.deltaTime);
+
+    }
+   
+}
